7186cfd2c75d9724f4f27cff49ed92de3b08f248
Marko Ristin <marko.ristin@gmail.com>
2021-03-31 21:49:38 +0200

    Add solution for AoC 2020 day 5
    This patch adds the solution for the Advent of Code 2020, day 5.

--- tests/test_day_2_password_philosophy.py
+++ tests/test_day_2_password_philosophy.py
@@ -7,23 +7,25 @@ import aocdbc.day_2_password_philosophy
 
 class TestDay2(unittest.TestCase):
     def test_cases(self) -> None:
-        test_cases = [
-            ('1-3 a: abcde', True),
-            ('1-3 b: cdefg', False),
-            ('2-9 c: ccccccccc', True)
+        cases = [
+            ("1-3 a: abcde", True),
+            ("1-3 b: cdefg", False),
+            ("2-9 c: ccccccccc", True),
         ]
 
-        for line, expected_result in test_cases:
+        for line, expected_result in cases:
             result = aocdbc.day_2_password_philosophy.verify_line(line=line)
             self.assertEqual(expected_result, result)
 
     def test_verify_with_icontract_hypothesis(self) -> None:
         icontract_hypothesis.test_with_inferred_strategy(
-            aocdbc.day_2_password_philosophy.verify)
+            aocdbc.day_2_password_philosophy.verify
+        )
 
     def test_verify_line_with_icontract_hypothesis(self) -> None:
         icontract_hypothesis.test_with_inferred_strategy(
-            aocdbc.day_2_password_philosophy.verify_line)
+            aocdbc.day_2_password_philosophy.verify_line
+        )
 
 
 if __name__ == "__main__":
