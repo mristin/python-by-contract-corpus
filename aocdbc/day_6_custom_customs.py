bbb2662b49a6a5d049ed43055d74a995964c3970
Lauren <debruynlauren1@gmail.com>
2021-04-02 08:38:31 +0200

    reformatted day 6

--- aocdbc/day_6_custom_customs.py
+++ aocdbc/day_6_custom_customs.py
@@ -2,9 +2,6 @@ from icontract import ensure, require
 import regex as re
 
 
-s = "abc\nab\n\na\nb\nbcd"
-
-
 @require(lambda input_string: all(re.match(r'^[a-z]*$', line) for line in input_string.split('\n')))
 @require(lambda input_string: all(len(set(list(line))) == len(line) for line in input_string.split('\n')))
 @ensure(lambda result: result >= 0)
@@ -16,11 +13,6 @@ def solve(input_string: str) -> int:
             yes_answers = yes_answers.union(list(line))
         else:
             count += len(yes_answers)
-            yes_answers = set()
+            yes_answers.clear()
     count += len(yes_answers)
     return count
-
-
-if __name__ == '__main__':
-    with open('../../AoC/day6_example.txt') as f:
-        print(solve(f.read()))
