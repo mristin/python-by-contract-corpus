ae2a1986bb6e05ddce0028bb507df813bc966ffa
Marko Ristin <marko.ristin@gmail.com>
2021-04-06 00:34:53 +0200

    Introduce classes for AoC day 22
    This patch introduces classes into Advent of Code day 22 "Crab Combat"
    so that the pre-conditions do not need to be duplicated.

--- aocdbc/day_22_crab_combat.py
+++ aocdbc/day_22_crab_combat.py
@@ -1,58 +1,69 @@
-import collections
 import re
-from typing import Tuple, List, Optional, Set, MutableMapping
+from typing import Tuple, List
 
-from icontract import require, ensure
+from icontract import require, ensure, DBC
 
 
 # crosshair: on
 
+
+class Deck(DBC, List[int]):
+    @require(lambda cards: all(card >= 0 for card in cards))
+    @require(lambda cards: len(set(cards)) == len(cards), "Unique cards")
+    def __init__(self, cards: List[int]) -> None:
+        list.__init__(self, cards)
+
+
+class Split(DBC):
+    @require(
+        lambda deck1, deck2: not set(deck1).intersection(deck2), "No overlapping cards"
+    )
+    def __init__(self, deck1: Deck, deck2: Deck) -> None:
+        self.deck1 = deck1
+        self.deck2 = deck2
+
+
 # fmt: off
-@require(
-    lambda deck1, deck2: not set(deck1).intersection(deck2),
-    "No overlapping cards"
-)
-@require(lambda deck1: len(deck1) > 0, "Not game over for player 1")
-@require(lambda deck2: len(deck2) > 0, "Not game over for player 2")
-@require(lambda deck1: len(set(deck1)) == len(deck1), "Unique cards")
-@require(lambda deck2: len(set(deck2)) == len(deck2), "Unique cards")
+@require(lambda split: len(split.deck1) > 0, "Not game over for player 1")
+@require(lambda split: len(split.deck2) > 0, "Not game over for player 2")
 @ensure(
-    lambda deck1, deck2, result:
-    set(deck1).union(deck2) == set(result[0]).union(result[1]),
+    lambda split, result:
+    set(split.deck1).union(split.deck2) == set(result.deck1).union(result.deck2),
     "No new cards"
 )
 @ensure(
-    lambda deck1, result:
-    deck1[1:] == result[0][0:len(deck1) - 1],
+    lambda split, result:
+    split.deck1[1:] == result.deck1[0:len(split.deck1) - 1],
     "Only the prefix and the suffix of the deck 1 change"
 )
 @ensure(
-    lambda deck2, result:
-    deck2[1:] == result[1][0:len(deck2) - 1],
+    lambda split, result:
+    split.deck2[1:] == result.deck2[0:len(split.deck2) - 1],
     "Only the prefix and the suffix of the deck 2 change"
 )
 @ensure(
-    lambda deck1, deck2, result:
-    (len(deck1) == len(result[0]) + 1 and len(deck2) == len(result[1]) - 1)
-    or (len(deck1) == len(result[0]) - 1 and len(deck2) == len(result[1]) + 1),
+    lambda split, result:
+    (
+            len(split.deck1) == len(result.deck1) + 1
+            and len(split.deck2) == len(result.deck2) - 1)
+    or (
+            len(split.deck1) == len(result.deck1) - 1
+            and len(split.deck2) == len(result.deck2) + 1),
     "Either lost or won two cards"
 )
 # fmt: on
-def play_a_round(deck1: List[int], deck2: List[int]) -> Tuple[List[int], List[int]]:
-    if len(deck1) == 0 or len(deck2) == 0:
-        return deck1, deck2
-
-    card1 = deck1[0]
-    card2 = deck2[0]
+def play_a_round(split: Split) -> Split:
+    card1 = split.deck1[0]
+    card2 = split.deck2[0]
 
     if card1 > card2:
-        new_deck1 = deck1[1:] + [card1, card2]
-        new_deck2 = deck2[1:]
+        new_deck1 = split.deck1[1:] + [card1, card2]
+        new_deck2 = split.deck2[1:]
     else:
-        new_deck1 = deck1[1:]
-        new_deck2 = deck2[1:] + [card2, card1]
+        new_deck1 = split.deck1[1:]
+        new_deck2 = split.deck2[1:] + [card2, card1]
 
-    result = (new_deck1, new_deck2)
+    result = Split(deck1=Deck(new_deck1), deck2=Deck(new_deck2))
     return result
 
 
@@ -68,8 +79,6 @@ def play_a_round(deck1: List[int], deck2: List[int]) -> Tuple[List[int], List[in
 @require(lambda lines: 'Player 2:' in lines[1:])
 @require(lambda lines: lines[0] == 'Player 1:')
 @require(lambda lines: len(lines) > 3)
-@ensure(lambda result: all(card >= 0 for card in result[0]))
-@ensure(lambda result: all(card >= 0 for card in result[1]))
 def parse_lines(lines: List[str]) -> Tuple[List[int], List[int]]:
     deck1 = []  # type: List[int]
     deck2 = []  # type: List[int]
@@ -87,9 +96,8 @@ def parse_lines(lines: List[str]) -> Tuple[List[int], List[int]]:
     return deck1, deck2
 
 
-@require(lambda deck: all(card >= 0 for card in deck))
 @ensure(lambda result: result >= 0)
-def compute_score(deck: List[int]) -> int:
+def compute_score(deck: Deck) -> int:
     score = 0
     for i, card in enumerate(deck):
         score += (len(deck) - i) * card
@@ -97,35 +105,30 @@ def compute_score(deck: List[int]) -> int:
     return score
 
 
-# (mristin, 2021-04-05): Do *not* introduce classes. It is easier to copy/paste
-# the contracts here. The classes resulted in really hard-to-follow contracts and
-# implementation.
 # fmt: off
-@require(lambda deck1: all(card >= 0 for card in deck1))
-@require(lambda deck2: all(card >= 0 for card in deck2))
-@require(
-    lambda deck1, deck2: not set(deck1).intersection(deck2),
-    "No overlapping cards"
-)
-@require(lambda deck1: len(deck1) > 0, "Not game over for player 1")
-@require(lambda deck2: len(deck2) > 0, "Not game over for player 2")
-@require(lambda deck1: len(set(deck1)) == len(deck1), "Unique cards")
-@require(lambda deck2: len(set(deck2)) == len(deck2), "Unique cards")
+@require(lambda split: len(split.deck1) > 0, "Not game over for player 1")
+@require(lambda split: len(split.deck2) > 0, "Not game over for player 2")
 @ensure(
-    lambda deck1, deck2, result:
-    (len(deck1) + len(deck2) == len(result[0]) and len(result[1] == 0))
-    or (len(result[0]) == 0 and len(deck1) + len(deck2) == len(result[1]))
+    lambda split, result:
+    (
+            len(split.deck1) + len(split.deck2) == len(result.deck1)
+            and len(result.deck2) == 0
+    )
+    or (
+            len(result.deck1) == 0
+            and len(split.deck1) + len(split.deck2) == len(result.deck2)
+    )
 )
 @ensure(
-    lambda deck1, deck2, result:
-    set(deck1).union(deck2) == set(result[0]).union(result[1])
+    lambda split, result:
+    set(split.deck1).union(split.deck2) == set(result.deck1).union(result.deck2)
 )
 # fmt: on
-def play(deck1: List[int], deck2: List[int]) -> Tuple[List[int], List[int]]:
+def play(split: Split) -> Split:
     while True:
-        deck1, deck2 = play_a_round(deck1=deck1, deck2=deck2)
+        split = play_a_round(split=split)
 
-        if len(deck1) == 0 or len(deck2) == 0:
+        if len(split.deck1) == 0 or len(split.deck2) == 0:
             break
 
-    return deck1, deck2
+    return split
