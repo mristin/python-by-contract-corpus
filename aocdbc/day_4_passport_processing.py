5543f29645474f1446a72317153d6a08f14bdb79
Phillip Schanely <pschanely@gmail.com>
2021-04-12 22:43:43 -0400

    Make is_kv_valid2 return bool
    Previously, we let it return any truthy/falsey value.

--- aocdbc/day_4_passport_processing.py
+++ aocdbc/day_4_passport_processing.py
@@ -65,37 +65,38 @@ def is_valid2(entry: Dict[str, str]) -> bool:
 
 
 @ensure(lambda: True)
-def is_kv_valid2(key: str, value: str) -> object:
+def is_kv_valid2(key: str, value: str) -> bool:
+    is_year = fullmatch(r"\d{4}", value)
     if key == "byr":
         # four digits; at least 1920 and at most 2002.
-        return fullmatch(r"\d{4}", value) and (1920 <= int(value) <= 2002)
+        return bool(is_year and (1920 <= int(value) <= 2002))
     elif key == "iyr":
         # four digits; at least 2010 and at most 2020.
-        return fullmatch(r"\d{4}", value) and (2010 <= int(value) <= 2020)
+        return bool(is_year and (2010 <= int(value) <= 2020))
     elif key == "eyr":
         # four digits; at least 2020 and at most 2030.
-        return fullmatch(r"\d{4}", value) and (2020 <= int(value) <= 2030)
+        return bool(is_year and (2020 <= int(value) <= 2030))
     elif key == "hgt":
         # a number followed by either cm or in:
         #  If cm, the number must be at least 150 and at most 193.
         #  If in, the number must be at least 59 and at most 76.
         if fullmatch(r".*cm", value):
             num = value[:-2]
-            return fullmatch(r"\d+", num) and (150 <= int(num) <= 193)
+            return bool(fullmatch(r"\d+", num) and (150 <= int(num) <= 193))
         elif fullmatch(r".*in", value):
             num = value[:-2]
-            return fullmatch(r"\d+", num) and (59 <= int(num) <= 76)
+            return bool(fullmatch(r"\d+", num) and (59 <= int(num) <= 76))
         else:
             return False
     elif key == "hcl":
-        return fullmatch(r"#(\d|[a-f]){6}", value)
+        return bool(fullmatch(r"#(\d|[a-f]){6}", value))
         # a "#" followed by exactly six characters 0-9 or a-f.
     elif key == "ecl":
         # exactly one of: amb blu brn gry grn hzl oth.
         return value in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
     elif key == "pid":
         # a nine-digit number, including leading zeroes.
-        return fullmatch(r"[0-9]{9}", value)
+        return bool(fullmatch(r"[0-9]{9}", value))
     return True
 
 
