dbdfadbd8da2f6d9eedb9a972edb42a3391c5be5
Marko Ristin <marko.ristin@gmail.com>
2021-05-02 14:56:08 +0200

    Add ETHZ EProg 2019 exercises 5 and 6
    These are only the problem statements, no solutions.

--- correct_programs/ethz_eprog_2019/__init__.py
+++ correct_programs/ethz_eprog_2019/__init__.py
@@ -10,24 +10,51 @@ Additionally, we skipped the exercises such as the Exercise 0 which introduced
 the students to version control and usage of IDEs. As the course was meant to teach
 Java, some exercises focused on Java-specific topics. Since we are collecting a corpus
 of Python programs annotated with contracts, we skipped such exercises as we did not
-deem them relevant for the corpus. Finally, we excluded the problems which we thought
-were too trivial for the corpus such as dealing with basic input/output operations
-(such as Exercise 2, Problem 4).
+deem them relevant for the corpus. Some of the exercises involve programming of the
+graphical user interfaces (GUIs). While contracts are indeed very useful in the GUI
+programming, we consider GUI programming to be out-of-scope for the current corpus.
+Finally, we excluded the problems which we thought were too simple such as dealing with
+basic input/output operations (such as Exercise 2, Problem 4).
 
-The original text of the problems is much longer and more elaborate. We focus on the
-source code, so we shortened the text as much as possible and give it here only as
-orientation about the complexity of the exercises. Please refer to the original text
-(in German) for details.
+The original problem statements are much longer and more elaborate. As our focus is on
+the source code, we shortened the text as much as possible and give it here only as
+orientation about the complexity of the exercises. The problems themselves were
+sometimes simplified to make for a more pointed and easier-to-read code (*e.g.*,
+exercise 6, problem 5). We explicitly marked the corresponding changes in
+the description of the problem.
 
-The content of the exercises is courtesy of Laboratory for Software Technology,
+Please refer to the original text (in German) for more details about the exercises.
+
+The original content of the exercises is courtesy of Laboratory for Software Technology,
 ETH Zurich, which also bears the copyright.
 
 You can find the original exercises at:
 https://www.lst.inf.ethz.ch/education/archive/Fall2019/einfuehrung-in-die-programmierung-i--252-0027-.html
 
-The list of skipped problems:
+We provide in the following the complete list of skipped problems and the reason for
+exclusion:
+
+* Exercise 0 and 1 are introductory exercises about how to use version control and IDE.
+* Exercise 2
+    * Problem 1 introduces general debugging practices in IDE.
+    * Problem 4 teaches console I/O which is too simple for contracts.
 
 * Exercise 4
-    * Problem 4: focuses on programming graphical user interfaces (GUIs) in Java.
+    * Problem 4 focuses on programming graphical user interfaces (GUIs) in Java.
+
+* Exercise 5
+    * Problem 1 concerns unit testing with JUnit.
+    * Problem 2 is too simple for the contracts.
+    * Problem 5 is a GUI exercise.
+
+* Exercise 6
+    * Problem 2 is Java-specific (introducing how to work with constructors and
+      classes). In our opinion, it was an extension of Exercise 5, problem 3 which
+      does not bring enough material in terms of contracts to merit the inclusion
+      in the corpus.
+
+    * Problem 3 is about black-box testing with JUnit, hence no solution can be written
+      in Python. However, it would be interesting in the future work to see how
+      contracts can help black-box testing.
 
 """
---
+++ correct_programs/ethz_eprog_2019/exercise_05/problem_03.py
@@ -0,0 +1,9 @@
+"""
+Analyze the height data of a population.
+
+The data is available at: http://jse.amstat.org/v11n2/datasets.heinz.html.
+
+Given a series of measurements in centimeters, compute:
+* The minimum, the maximum and average height of the population,
+* A histogram of the heights, given a bin width ``b``.
+"""
