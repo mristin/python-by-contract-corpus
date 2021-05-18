a47f33c6514c8de71ee398feb370adf3af58886e
Marko Ristin <marko@ristin.ch>
2021-05-18 08:40:39 +0200

    Solve ETHZ Eprog 2019 Exercise 06 (#63)
    This patch adds both the solutions and the tests.

--- correct_programs/ethz_eprog_2019/exercise_06/problem_01.py
+++ correct_programs/ethz_eprog_2019/exercise_06/problem_01.py
@@ -17,10 +17,15 @@ The "Swiss time" goes only from 1 to 12, but different periods of the day are in
     21:51 -> 9 vor 10i am abig
     22:37 -> 7 ab halbi 11i znacht
 
-"Am morge" is before 12pm. 12pm is "mittag". "Namittag is till 6pm. "Abig" goes till
-10pm. The remainder of the day is "znacht".
+The period of day is determined as follows:
 
-If the number of minutes (``mm``) is greater-equal 25 25, the hours are incremented by
+* "Am morge" is after 6am and before 12pm.
+* 12pm is "mittag".
+* "Namittag is till 6pm.
+* "Abig" goes till 10pm.
+* The remainder of the day is "znacht".
+
+If the number of minutes (``mm``) is greater-equal 25, the hours are incremented by
 1 ("5 vor halbi 10i").
 
 If there are less than 25 minutes, you say "ab", but otherwise "vor". Between
@@ -29,3 +34,54 @@ the minutes 25 and 39, you say "vor halbi" or "ab halbi". If it is exactly 15, 3
 
 For the hours after 3, you need to put a suffix "i" ("viertel vor 2", but "1 ab 12i").
 """
+
+from icontract import require
+
+
+@require(lambda hour: 0 <= hour < 23)
+@require(lambda minute: 0 <= minute < 60)
+def time_in_swiss_german(hour: int, minute: int) -> str:
+    if hour < 6:
+        period_of_day = "znacht"
+    elif 6 <= hour < 12:
+        period_of_day = "am morge"
+    elif hour == 12:
+        period_of_day = "am mittag"
+    elif hour < 18:
+        period_of_day = "am namittag"
+    elif hour < 22:
+        period_of_day = "am abig"
+    else:
+        period_of_day = "znacht"
+
+    if hour == 0:
+        clock_hour = 12
+    elif hour > 12:
+        clock_hour = hour - 12
+    else:
+        clock_hour = hour
+    hour_suffix = "i" if clock_hour > 3 else ""
+
+    if minute == 0:
+        return f"{clock_hour}{hour_suffix} {period_of_day}"
+
+    stated_hour = clock_hour if minute < 25 else clock_hour + 1
+
+    if minute == 15:
+        minute_part = "viertel ab"
+    elif minute == 30:
+        minute_part = "halbi"
+    elif minute == 45:
+        minute_part = "viertel vor"
+    elif minute < 25:
+        minute_part = f"{minute} ab"
+    elif 25 <= minute < 30:
+        minute_part = f"{30 - minute} vor halbi"
+    elif 30 < minute <= 39:
+        minute_part = f"{minute - 30} ab halbi"
+    else:
+        minute_part = f"{60 - minute} vor"
+
+    result = f"{minute_part} {stated_hour}{hour_suffix} {period_of_day}"
+
+    return result
--- correct_programs/ethz_eprog_2019/exercise_06/problem_05.py
+++ correct_programs/ethz_eprog_2019/exercise_06/problem_05.py
@@ -10,3 +10,49 @@ of the problem. The original problem additionally concerns the drawing of the cl
 hands in the GUI which we intentionally leave out as Java-specific and not relevant
 in terms of contracts.)
 """
+from typing import Tuple
+
+from icontract import require, ensure
+
+# fmt: off
+@require(lambda hour: 0 <= hour <= 23)
+@require(lambda minute: 0 <= minute < 60)
+@require(lambda second: 0 <= second < 60)
+@ensure(lambda result: all(0 <= angle < 360 for angle in result))
+@ensure(
+    lambda hour, minute, second, result:
+    not (hour == 0 and minute == 0 and second == 0)
+    or result[0] == 0 and result[1] == 0 and result[2] == 0,
+    "Angles start from 00:00:00")
+@ensure(
+    lambda hour, minute, second, result:
+    not (hour == 0 and minute == 0 and second > 0)
+    or result[0] > 0 and result[1] > 0 and result[2] > 0,
+    "All hands of a clock move when the hand for seconds moves"
+)
+@ensure(
+    lambda hour, result: hour / 12 * 360 <= result[0] < (hour + 1) / 12 * 360,
+    "Hour hand between two hour ticks"
+)
+@ensure(
+    lambda minute, result: minute / 60 * 360 <= result[1] < (minute + 1) / 60 * 360,
+    "Minute hand between two minute ticks"
+)
+@ensure(
+    lambda second, result: second / 60 * 360 <= result[2] < (second + 1) / 60 * 360,
+    "Second hand between two second ticks"
+)
+@ensure(
+    lambda hour, minute, second, result:
+    not (minute == 0 and second == 0)
+    or result[0] == hour / 12 * 360 and result[1] == 0 and result[2] == 0
+)
+# fmt: on
+def compute_angles(hour: int, minute: int, second: int) -> Tuple[float, float, float]:
+    angle_second = second / 60 * 360
+
+    angle_minute = (minute + second / 60) / 60 * 360
+
+    angle_hour = (hour + minute / 60 + second / 3600) / 12 * 360
+
+    return angle_hour, angle_minute, angle_second
