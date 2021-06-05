b68ea59b962f105182d3bf57fcd6e23f9b815ddc
Phillip Schanely <pschanely@gmail.com>
2021-06-05 10:52:09 -0400

    Solve ETHZ Eprog 2019 Exercise 08 Problem 01
    This patch adds both the solutions and the tests.

--- correct_programs/ethz_eprog_2019/exercise_08/problem_01.py
+++ correct_programs/ethz_eprog_2019/exercise_08/problem_01.py
@@ -16,3 +16,35 @@ Here are a couple of examples:
 * ``s = "abc"
 
 """
+
+from typing import Counter, Set
+
+from icontract import require, ensure
+
+
+@require(lambda dist: dist >= 0)
+@ensure(lambda src, chars, result: result is False if len(chars) > len(src) else True)
+@ensure(
+    lambda src, chars, result: (result is True)
+    if (sorted(chars) in sorted(src))
+    else True
+)
+@ensure(lambda src, chars, result: (result is True) if (chars in src) else True)
+def matches(src: str, chars: str, dist: int) -> bool:
+    return stretch(src, Counter(chars), len(src) - len(chars), dist)
+
+
+def stretch(src: str, chars: Counter[str], initialdist: int, dist: int) -> bool:
+    if sum(chars.values()) == 0:
+        return True
+    prefix = src[: initialdist + 1]
+    if all(chars[ch] == 0 for ch in prefix):
+        return False
+    for idx, ch in enumerate(prefix):
+        if chars[ch] <= 0:
+            continue
+        subch = chars.copy()
+        subch[ch] -= 1
+        if stretch(src[idx + 1 :], subch, dist, dist):
+            return True
+    return False
