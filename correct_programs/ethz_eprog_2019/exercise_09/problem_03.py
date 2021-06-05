29467ab093127528d9c89cda045860081a35f11f
Marko Ristin <marko.ristin@gmail.com>
2021-06-05 09:17:59 +0200

    Solve ETHZ Eprog 2019 Exercise 09 Problem 03

--- correct_programs/ethz_eprog_2019/exercise_09/problem_03.py
+++ correct_programs/ethz_eprog_2019/exercise_09/problem_03.py
@@ -33,3 +33,145 @@ For the aforementioned example:
     Michaela Meier: 13799
     Stefan Oliver Schmid: 12808
 """
+import datetime
+import enum
+import re
+from typing import Mapping, List, Optional, MutableMapping, Pattern, AnyStr
+
+from icontract import require, ensure, DBC
+
+from correct_programs.common import Lines
+
+
+class ClassOfFlight(enum.Enum):
+    ECONOMY = "ECONOMY"
+    BUSINESS = "BUSINESS"
+    FIRST = "FIRST"
+
+
+STR_TO_CLASS_OF_FLIGHT = {
+    entry.value: entry for entry in ClassOfFlight
+}  # type: Mapping[str, ClassOfFlight]
+
+
+class Flight(DBC):
+    @require(lambda miles: miles is None or miles > 0)
+    @ensure(lambda miles, self: not (miles is None) or self.miles == 125)
+    def __init__(
+        self,
+        number: str,
+        date: datetime.date,
+        class_of_flight: ClassOfFlight,
+        miles: Optional[int],
+    ) -> None:
+        self.number = number
+        self.date = date
+        self.class_of_flight = class_of_flight
+        self.miles = miles if miles is not None else 125
+
+
+class Block(DBC):
+    def __init__(self, name: str, flights: List[Flight]):
+        self.name = name
+        self.flights = flights
+
+
+def compile_flight_re() -> Pattern[AnyStr]:
+    number = r"(?P<number>[a-zA-Z0-9]+)"
+    date = r"(?P<date>[0-9]{2}.[0-9]{2}.[0-9]{4})"
+    class_of_flight = "".join(
+        ["(?P<class_of_flight>"]
+        + ["|".join(re.escape(enum_entry.value) for enum_entry in ClassOfFlight)]
+        + [")"]
+    )
+
+    miles = r"( (?P<miles>[0-9]+))?"
+
+    return re.compile(f"^{number} {date} {class_of_flight}{miles}$")  # type: ignore
+
+
+FLIGHT_RE = compile_flight_re()
+
+
+# fmt: off
+@require(lambda lines: len(lines) >= 1)
+@require(lambda lines: all(FLIGHT_RE.match(line) for line in lines[1:]))
+@ensure(lambda lines, result: result.name == lines[0])
+@ensure(
+    lambda lines, result:
+    not (len(lines) == 1) or len(result.flights) == 0
+)
+# fmt: on
+def parse_block(lines: Lines) -> Block:
+    name = lines[0]
+    flights = []  # type: List[Flight]
+
+    for line in lines[1:]:
+        mtch = FLIGHT_RE.match(line)
+        assert mtch
+
+        flights.append(
+            Flight(
+                number=mtch.group("number"),
+                date=datetime.datetime.strptime(mtch.group("date"), "%d.%m.%Y").date(),
+                class_of_flight=STR_TO_CLASS_OF_FLIGHT[mtch.group("class_of_flight")],
+                miles=None if mtch.group("miles") is None else int(mtch.group("miles")),
+            )
+        )
+
+    return Block(name=name, flights=flights)
+
+
+@ensure(lambda lines, result: not (len(lines) == 0) or len(result) == 0)
+@ensure(lambda lines, result: not (len(lines) > 0) or len(result) > 0)
+def parse(lines: Lines) -> List[Block]:
+    list_of_block_lines = []  # type: List[Lines]
+
+    accumulator = []  # type: List[str]
+    for line in lines:
+        print(f"line is {line!r}")  # TODO: debug
+        if line == ".":
+            if accumulator:
+                list_of_block_lines.append(Lines(accumulator))
+                accumulator = []
+        else:
+            accumulator.append(line)
+
+    if accumulator:
+        raise ValueError(
+            "Unexpected unfinished block: {}".format("\n".join(accumulator))
+        )
+
+    return [parse_block(lines=block_lines) for block_lines in list_of_block_lines]
+
+
+# fmt: off
+@ensure(lambda result: all(value >= 0 for value in result.values()))
+@ensure(
+    lambda blocks, result:
+    set(block.name for block in blocks) == set(result.keys()),
+    "All people considered"
+)
+@ensure(
+    lambda blocks, result:
+    not (len(blocks) != 0) or len(result) > 0
+)
+# fmt: on
+def compute_totals(blocks: List[Block]) -> MutableMapping[str, int]:
+    result = dict()  # type: MutableMapping[str, int]
+
+    for block in blocks:
+        if block.name not in result:
+            result[block.name] = 0
+
+        for flight in block.flights:
+            if flight.class_of_flight == ClassOfFlight.ECONOMY:
+                result[block.name] += flight.miles
+            elif flight.class_of_flight == ClassOfFlight.BUSINESS:
+                result[block.name] += flight.miles * 2
+            elif flight.class_of_flight == ClassOfFlight.FIRST:
+                result[block.name] += flight.miles * 3
+            else:
+                raise NotImplementedError(flight.class_of_flight)
+
+    return result
