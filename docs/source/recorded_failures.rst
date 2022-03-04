f316234bff1ee47cdc62697245c7de0a43e4e7eb
Marko Ristin <marko.ristin@gmail.com>
2022-03-04 08:37:35 +0100

    Move recordings to `archived/` to avoid confusion (#112)
    There was some confusion where the curated incorrect programs reside,
    see #110. Therefore, we moved the recorded failures to a separate
    folder, `archived/` to signal that they should be used only for
    histology, while we refined the docs to point directly to the curated
    incorrect programs.

--- docs/source/recorded_failures.rst
+++ docs/source/recorded_failures.rst
@@ -5,19 +5,18 @@ Recorded Failures
 As we developed the solutions to the exercises, we recorded failures detected by the our tools.
 These records provide a material for education, further research and initial insights what bugs could be (easily) detected.
 
-The recorded failures can be found at: https://github.com/mristin/python-by-contract-corpus/tree/main/recorded_failures
-
-Important Notes
-===============
-Form
-----
-For the time being, the recorded failures consist of the source codes *at the time* of the failure.
+Curated Incorrect Programs
+==========================
+The recorded failures consist of the source codes *at the time* of the failure.
 This is not really optimal as it is hard to compare the working version of the recorded buggy source code with the final solution.
-In the future, we want to re-phrase the buggy code by introducing the bug into the final solution.
+
+We re-phrased the buggy code by introducing the bug into the final solution.
 The re-phrasing should allow the reader to easily differentiate the buggy from the final solution.
 
-Bias
-----
+The thus curated incorrect programs with bugs can be found at: https://github.com/mristin/python-by-contract-corpus/tree/main/python_by_contract_corpus/incorrect_from_recorded
+
+Note about the Bias
+===================
 The mistakes that practically occur during the development are highly biased towards the skill level of the developer as well as his/her familiarity with the testing tools.
 Since we were very few contributors, and the distribution of problem assignment was skewed towards two contributors rather than random, the current corpus is inevitably highly biased towards the dominant contributors.
 
--- python_by_contract_corpus/incorrect_from_recorded/__init__.py
+++ python_by_contract_corpus/incorrect_from_recorded/__init__.py
@@ -9,31 +9,31 @@ the (apparently correct) final solutions.
 We could not reproduce the following failure cases since they were either not
 informative enough or the final and the recorded solution diverged too much:
 
-* recorded_failures/aoc2020/day_3_toboggan_trajectory/empty_line_at_end_when_split_on_newline.py
-* recorded_failures/aoc2020/day_7_handy_haversacks/dont_count_outer_bag.py
-* recorded_failures/aoc2020/day_9_encoding_error/postcondition_cannot_handle_nonetype.py
-* recorded_failures/aoc2020/day_11_seating_system/invalid_state_change_caught_by_contracts.py
-* recorded_failures/aoc2020/day_12_rain_risk/invalid_orientation.py
-* recorded_failures/aoc2020/day_12_rain_risk/regex_incorrectly_ended.py
-* recorded_failures/aoc2020/day_18_operation_order/empty_expressions.py
-* recorded_failures/aoc2020/day_18_operation_order/empty_tails.py
-* recorded_failures/aoc2020/day_18_operation_order/negative_numbers.py
-* recorded_failures/aoc2020/day_22_crab_combat/play_a_round_failed_on_edge_case.py
-* recorded_failures/aoc2020/day_22_crab_combat/play_a_round_failed_on_another_edge_case.py
-* recorded_failures/aoc2020/day_25_combo_breaker/forgot_to_return.py
-* recorded_failures/ethz_eprog_2019/exercise_04/problem_02/missed_edge_case.py
-* recorded_failures/ethz_eprog_2019/exercise_06/problem_04/forgot_an_edge_case_when_checking_same_order_on_empty_list.py
-* recorded_failures/ethz_eprog_2019/exercise_06/problem_04/forgot_to_sync_first_and_last.py
-* recorded_failures/ethz_eprog_2019/exercise_06/problem_04/got_add_last_wrong.py
-* recorded_failures/ethz_eprog_2019/exercise_07/problem_02/got_split_wrong.py
-* recorded_failures/ethz_eprog_2019/exercise_08/problem_03/got_leaving_customer_wrong.py
-* recorded_failures/ethz_eprog_2019/exercise_11/problem_01/missed_edge_case_in_postcondition_where_all_grades_are_zero.py
-* recorded_failures/ethz_eprog_2019/exercise_11/problem_02/got_startpos_endpos_wrong_in_tokenization.py
-* recorded_failures/ethz_eprog_2019/exercise_11/problem_02/missing_edge_cases_for_evaluation.py
+* archived/recorded_failures/aoc2020/day_3_toboggan_trajectory/empty_line_at_end_when_split_on_newline.py
+* archived/recorded_failures/aoc2020/day_7_handy_haversacks/dont_count_outer_bag.py
+* archived/recorded_failures/aoc2020/day_9_encoding_error/postcondition_cannot_handle_nonetype.py
+* archived/recorded_failures/aoc2020/day_11_seating_system/invalid_state_change_caught_by_contracts.py
+* archived/recorded_failures/aoc2020/day_12_rain_risk/invalid_orientation.py
+* archived/recorded_failures/aoc2020/day_12_rain_risk/regex_incorrectly_ended.py
+* archived/recorded_failures/aoc2020/day_18_operation_order/empty_expressions.py
+* archived/recorded_failures/aoc2020/day_18_operation_order/empty_tails.py
+* archived/recorded_failures/aoc2020/day_18_operation_order/negative_numbers.py
+* archived/recorded_failures/aoc2020/day_22_crab_combat/play_a_round_failed_on_edge_case.py
+* archived/recorded_failures/aoc2020/day_22_crab_combat/play_a_round_failed_on_another_edge_case.py
+* archived/recorded_failures/aoc2020/day_25_combo_breaker/forgot_to_return.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_04/problem_02/missed_edge_case.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_06/problem_04/forgot_an_edge_case_when_checking_same_order_on_empty_list.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_06/problem_04/forgot_to_sync_first_and_last.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_06/problem_04/got_add_last_wrong.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_07/problem_02/got_split_wrong.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_08/problem_03/got_leaving_customer_wrong.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_11/problem_01/missed_edge_case_in_postcondition_where_all_grades_are_zero.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_11/problem_02/got_startpos_endpos_wrong_in_tokenization.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_11/problem_02/missing_edge_cases_for_evaluation.py
 
 Additionally, we excluded the following failures since they were underspecified in
 the exercise description:
 
-* recorded_failures/ethz_eprog_2019/exercise_12/problem_03/forgot_to_specify_what_happens_if_the_result_is_too_large.py
-* recorded_failures/ethz_eprog_2019/exercise_12/problem_03/forgot_to_specify_what_happens_on_math_errors_in_function_call.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_12/problem_03/forgot_to_specify_what_happens_if_the_result_is_too_large.py
+* archived/recorded_failures/ethz_eprog_2019/exercise_12/problem_03/forgot_to_specify_what_happens_on_math_errors_in_function_call.py
 """
